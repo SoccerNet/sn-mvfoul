99a4c33cef07768443e56ea18b951265dc463a9f
Held Jan <78489470+heldJan@users.noreply.github.com>
2023-05-02 10:02:53 +0200

    Add files via upload

---
+++ VARS interface/interface/mvaggregate.py
@@ -0,0 +1,81 @@
+"""
+MIT License
+
+Copyright (c) 2022 Abdullah Hamdi
+
+The architecture was taken from MVTorch (https://github.com/ajhamdi/mvtorch).
+"""
+
+
+from interface.utils import batch_tensor, unbatch_tensor, class_freq_to_weight, torch_direction_vector, labels2freq
+import torch
+import numpy 
+from torch import dropout, nn
+from mvtorch.models.voint import *
+from einops import rearrange, repeat
+
+
+class ViewMaxAggregate(nn.Module):
+    def __init__(self,  model, lifting_net=nn.Sequential()):
+        super().__init__()
+        self.model = model
+        self.lifting_net = lifting_net
+
+    def forward(self, mvimages):
+        B, V, C, D, H, W = mvimages.shape # Batch, Views, Channel, Depth, Height, Width
+        aux = self.lifting_net(unbatch_tensor(self.model(batch_tensor(mvimages, dim=1, squeeze=True)), B, dim=1, unsqueeze=True))
+        pooled_view = torch.max(aux, dim=1)[0]
+        return pooled_view.squeeze(), aux
+
+
+class ViewAvgAggregate(nn.Module):
+    def __init__(self,  model, lifting_net=nn.Sequential()):
+        super().__init__()
+        self.model = model
+        self.lifting_net = lifting_net
+
+    def forward(self, mvimages):
+        B, V, C, D, H, W = mvimages.shape # Batch, Views, Channel, Depth, Height, Width
+        aux = self.lifting_net(unbatch_tensor(self.model(batch_tensor(mvimages, dim=1, squeeze=True)), B, dim=1, unsqueeze=True))
+        pooled_view = torch.mean(aux, dim=1)
+        return pooled_view.squeeze(), aux
+
+
+class MVAggregate(nn.Module):
+    def __init__(self,  model, agr_type="max", feat_dim=400, num_classes=9,lifting_net=nn.Sequential(), sequential=True):
+        super().__init__()
+        self.agr_type = agr_type
+        self.sequential=sequential
+
+        self.inter = nn.Sequential(
+            nn.LayerNorm(feat_dim),
+            nn.Linear(feat_dim, feat_dim),
+            nn.Linear(feat_dim, feat_dim),
+        )
+
+        self.fc_offence = nn.Sequential(
+            nn.LayerNorm(feat_dim),
+            nn.Linear(feat_dim, feat_dim),
+            nn.Linear(feat_dim, 4)
+        )
+
+
+        self.fc_action = nn.Sequential(
+            nn.LayerNorm(feat_dim),
+            nn.Linear(feat_dim, feat_dim),
+            nn.Linear(feat_dim, 8)
+        )
+        if self.agr_type == "max":
+            self.aggregation_model = ViewMaxAggregate(model=model, lifting_net=lifting_net)
+        elif self.agr_type == "mean":
+            self.aggregation_model = ViewAvgAggregate(model=model, lifting_net=lifting_net)
+
+    def forward(self, mvimages):
+
+        pooled_view, before_pool = self.aggregation_model(mvimages)
+
+        inter1 = self.inter(pooled_view)
+        pred_action = self.fc_action(inter1)
+        pred_offence_severity = self.fc_offence(inter1)
+
+        return pred_offence_severity, pred_action
