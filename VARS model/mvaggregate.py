5eccc5e353f1e564edef7c3e501f9c7316c6ca13
Held Jan <78489470+heldJan@users.noreply.github.com>
2024-01-27 08:29:06 +0100

    Update mvaggregate.py

--- VARS model/mvaggregate.py
+++ VARS model/mvaggregate.py
@@ -1,28 +1,66 @@
-"""
-MIT License
-
-Copyright (c) 2022 Abdullah Hamdi
-
-The architecture was taken from MVTorch (https://github.com/ajhamdi/mvtorch).
-
-I changed the multi-view image architecture to a multi-view video architecture.
-"""
-
-from utils import batch_tensor, unbatch_tensor, class_freq_to_weight, torch_direction_vector, labels2freq
+from utils import batch_tensor, unbatch_tensor
 import torch
-import numpy as np
-import torchvision
-from torch import dropout, nn
-from mvtorch.models.voint import *
+from torch import nn
 
-from einops import rearrange, repeat
-from einops.layers.torch import Rearrange
+
+class WeightedAggregate(nn.Module):
+    def __init__(self,  model, feat_dim, lifting_net=nn.Sequential()):
+        super().__init__()
+        self.model = model
+        self.lifting_net = lifting_net
+        num_heads = 8
+        self.feature_dim = feat_dim
+
+        r1 = -1
+        r2 = 1
+        self.attention_weights = nn.Parameter((r1 - r2) * torch.rand(feat_dim, feat_dim) + r2)
+
+        self.normReLu = nn.Sequential(
+            nn.LayerNorm(feat_dim),
+            nn.ReLU()
+        )        
+
+        self.relu = nn.ReLU()
+   
+
+
+    def forward(self, mvimages):
+        B, V, C, D, H, W = mvimages.shape # Batch, Views, Channel, Depth, Height, Width
+        aux = self.lifting_net(unbatch_tensor(self.model(batch_tensor(mvimages, dim=1, squeeze=True)), B, dim=1, unsqueeze=True))
+
+
+        ##################### VIEW ATTENTION #####################
+
+        # S = source length 
+        # N = batch size
+        # E = embedding dimension
+        # L = target length
+
+        aux = torch.matmul(aux, self.attention_weights)
+        # Dimension S, E for two views (2,512)
+
+        # Dimension N, S, E
+        aux_t = aux.permute(0, 2, 1)
+
+        prod = torch.bmm(aux, aux_t)
+        relu_res = self.relu(prod)
+        
+        aux_sum = torch.sum(torch.reshape(relu_res, (B, V*V)).T, dim=0).unsqueeze(0)
+        final_attention_weights = torch.div(torch.reshape(relu_res, (B, V*V)).T, aux_sum.squeeze(0))
+        final_attention_weights = final_attention_weights.T
+
+        final_attention_weights = torch.reshape(final_attention_weights, (B, V, V))
+
+        final_attention_weights = torch.sum(final_attention_weights, 1)
+
+        output = torch.mul(aux.squeeze(), final_attention_weights.unsqueeze(-1))
+
+        output = torch.sum(output, 1)
+
+        return output.squeeze(), final_attention_weights
 
 
 class ViewMaxAggregate(nn.Module):
-    """
-    A function to aggregate multi-view features using max pooling.
-    """
     def __init__(self,  model, lifting_net=nn.Sequential()):
         super().__init__()
         self.model = model
@@ -36,9 +74,6 @@ class ViewMaxAggregate(nn.Module):
 
 
 class ViewAvgAggregate(nn.Module):
-    """
-    A function to aggregate multi-view features using mean pooling.
-    """
     def __init__(self,  model, lifting_net=nn.Sequential()):
         super().__init__()
         self.model = model
@@ -51,7 +86,7 @@ class ViewAvgAggregate(nn.Module):
         return pooled_view.squeeze(), aux
 
 
-class MVAggregate(nn.Module): 
+class MVAggregate(nn.Module):
     def __init__(self,  model, agr_type="max", feat_dim=400, lifting_net=nn.Sequential()):
         super().__init__()
         self.agr_type = agr_type
@@ -77,15 +112,17 @@ class MVAggregate(nn.Module):
 
         if self.agr_type == "max":
             self.aggregation_model = ViewMaxAggregate(model=model, lifting_net=lifting_net)
-        else:
+        elif self.agr_type == "mean":
             self.aggregation_model = ViewAvgAggregate(model=model, lifting_net=lifting_net)
+        else:
+            self.aggregation_model = WeightedAggregate(model=model, feat_dim=feat_dim, lifting_net=lifting_net)
 
     def forward(self, mvimages):
 
         pooled_view, attention = self.aggregation_model(mvimages)
 
-        inter1 = self.inter(pooled_view)
-        pred_action = self.fc_action(inter1)
-        pred_offence_severity = self.fc_offence(inter1)
+        inter = self.inter(pooled_view)
+        pred_action = self.fc_action(inter)
+        pred_offence_severity = self.fc_offence(inter)
 
         return pred_offence_severity, pred_action, attention
