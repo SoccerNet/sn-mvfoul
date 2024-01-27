89698e8b3e4900cbd70bab204b639e19dc2c9c4a
Held Jan <78489470+heldJan@users.noreply.github.com>
2024-01-27 08:31:00 +0100

    Update data_loader.py

--- VARS model/data_loader.py
+++ VARS model/data_loader.py
@@ -1,31 +1,23 @@
-from torch.utils.data import Dataset
 
-import numpy as np
-import random
 import os
-import time
-from tqdm import tqdm
-
 import torch
 import json
 from config.classes import EVENT_DICTIONARY
-from torchvision.io.video import read_video, read_video_timestamps
-import torch.nn as nn
+
+##############################################################
+#                                                            #
+#              DO NOT MAKE ANY CHANGES HERE                  #
+#                                                            #
+##############################################################
 
 
-###############################################
-# CLASS MERGIN
-###############################################
 
-def label2vectormerge(folder_path, split, types, num_views):
-	"""
-	A function to get the list of labels.
-	"""
+# Function to load the labels from the json file
+def label2vectormerge(folder_path, split, num_views):
 	path_annotations = os.path.join(folder_path, split)
 	path_annotations = os.path.join(path_annotations, "annotations.json") 
 
 	dictionary_action = EVENT_DICTIONARY['action_class']
-
 
 	if os.path.exists(path_annotations):
 		with open(path_annotations) as f:
@@ -41,6 +33,9 @@ def label2vectormerge(folder_path, split, types, num_views):
 
 	labels_action = []
 	labels_offence_severity= []
+	number_of_actions = []
+
+	total_distribution = torch.zeros(num_classes_offence_severity, num_classes_action)
 	distribution_action = torch.zeros(1, num_classes_action)
 	distribution_offence_severity = torch.zeros(1, num_classes_offence_severity)
 
@@ -48,6 +43,7 @@ def label2vectormerge(folder_path, split, types, num_views):
 		action_class = train_annotations_data['Actions'][actions]['Action class']
 		offence_class = train_annotations_data['Actions'][actions]['Offence']
 		severity_class = train_annotations_data['Actions'][actions]['Severity']
+
 
 		if action_class == '' or action_class == 'Dont know':
 			not_taking.append(actions)
@@ -57,7 +53,7 @@ def label2vectormerge(folder_path, split, types, num_views):
 			not_taking.append(actions)
 			continue
 
-		if (severity_class == '' or severity_class == '2.0' or severity_class == '4.0') and action_class != 'Dive' and offence_class != 'No offence':
+		if (severity_class == '' or severity_class == '2.0' or severity_class == '4.0') and action_class != 'Dive' and offence_class != 'No offence' and offence_class != 'No Offence':
 			not_taking.append(actions)
 			continue
 
@@ -69,60 +65,70 @@ def label2vectormerge(folder_path, split, types, num_views):
 
 		if num_views == 1:
 			for i in range(len(train_annotations_data['Actions'][actions]['Clips'])):
-				if offence_class == 'No offence':
+				if offence_class == 'No Offence' or offence_class == 'No offence':
 					labels_offence_severity.append(torch.zeros(1, num_classes_offence_severity))
 					labels_offence_severity[len(labels_offence_severity)-1][0][0] = 1
 					distribution_offence_severity[0][0] += 1
+					off_index = 0
 				elif offence_class == 'Offence' and severity_class == '1.0':
 					labels_offence_severity.append(torch.zeros(1, num_classes_offence_severity))
 					labels_offence_severity[len(labels_offence_severity)-1][0][1] = 1
 					distribution_offence_severity[0][1] += 1
+					off_index = 1
 				elif offence_class == 'Offence' and severity_class == '3.0':
 					labels_offence_severity.append(torch.zeros(1, num_classes_offence_severity))
 					labels_offence_severity[len(labels_offence_severity)-1][0][2] = 1
 					distribution_offence_severity[0][2] += 1
+					off_index = 2
 				elif offence_class == 'Offence' and severity_class == '5.0':
 					labels_offence_severity.append(torch.zeros(1, num_classes_offence_severity))
 					labels_offence_severity[len(labels_offence_severity)-1][0][3] = 1
 					distribution_offence_severity[0][3] += 1
+					off_index = 3
 				else:
 					not_taking.append(actions)
 					continue
 				labels_action.append(torch.zeros(1, num_classes_action))
 				labels_action[len(labels_action)-1][0][dictionary_action[action_class]] = 1
 				distribution_action[0][dictionary_action[action_class]] += 1
+				total_distribution[off_index][dictionary_action[action_class]] += 1
 		else:
-			if offence_class == 'No offence':
+			if offence_class == 'No Offence' or offence_class == 'No offence':
 				labels_offence_severity.append(torch.zeros(1, num_classes_offence_severity))
 				labels_offence_severity[len(labels_offence_severity)-1][0][0] = 1
 				distribution_offence_severity[0][0] += 1
+				index = 0
 			elif offence_class == 'Offence' and severity_class == '1.0':
 				labels_offence_severity.append(torch.zeros(1, num_classes_offence_severity))
 				labels_offence_severity[len(labels_offence_severity)-1][0][1] = 1
 				distribution_offence_severity[0][1] += 1
+				index = 1
 			elif offence_class == 'Offence' and severity_class == '3.0':
 				labels_offence_severity.append(torch.zeros(1, num_classes_offence_severity))
 				labels_offence_severity[len(labels_offence_severity)-1][0][2] = 1
 				distribution_offence_severity[0][2] += 1
+				index = 2
 			elif offence_class == 'Offence' and severity_class == '5.0':
 				labels_offence_severity.append(torch.zeros(1, num_classes_offence_severity))
 				labels_offence_severity[len(labels_offence_severity)-1][0][3] = 1
 				distribution_offence_severity[0][3] += 1
+				index = 3
 			else:
 				not_taking.append(actions)
 				continue
+
+			number_of_actions.append(actions)
 			labels_action.append(torch.zeros(1, num_classes_action))
 			labels_action[len(labels_action)-1][0][dictionary_action[action_class]] = 1
 			distribution_action[0][dictionary_action[action_class]] += 1
+			total_distribution[index][dictionary_action[action_class]] += 1
+
+	return labels_offence_severity, labels_action, distribution_offence_severity[0], distribution_action[0], not_taking, number_of_actions
 
 
-	return labels_offence_severity, labels_action, distribution_offence_severity[0], distribution_action[0], not_taking
+# Function to load the path to the clips
+def clips2vectormerge(folder_path, split, num_views, not_taking):
 
-
-def clips2vectormerge(folder_path, split, types, num_views, not_taking):
-	"""
-	A function to get a list of all the clips
-	"""
 	path_clips = os.path.join(folder_path, split)
 
 	if os.path.exists(path_clips):
@@ -177,4 +183,5 @@ def clips2vectormerge(folder_path, split, types, num_views, not_taking):
 				clips.append(clips_all_view)
 
 		return clips
+
 
