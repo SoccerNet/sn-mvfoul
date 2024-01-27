4691da01533e804282eb098e766f19abe52321d2
Held Jan <78489470+heldJan@users.noreply.github.com>
2024-01-27 08:29:50 +0100

    Update dataset.py

--- VARS model/dataset.py
+++ VARS model/dataset.py
@@ -1,29 +1,43 @@
 from torch.utils.data import Dataset
-
-import numpy as np
 from random import random
-import os
-import time
-from tqdm import tqdm
 import torch
-import logging
-import json
 import random
 from data_loader import label2vectormerge, clips2vectormerge
-from torchvision.io.video import read_video, write_video
-import time
-from torchvision.models.video import MViT_V2_S_Weights
+from torchvision.io.video import read_video
 
 
 class MultiViewDataset(Dataset):
-    def __init__(self, path, start, end, fps, split, types, num_views, action=False, shift=0, transform=None, transform_model=None):
+    def __init__(self, path, start, end, fps, split, num_views, transform=None, transform_model=None):
 
-        #load the annotations
-        self.labels_offence_severity, self.labels_action, self.distribution_offence_severity,self.distribution_action, not_taking = label2vectormerge(path, split, types, num_views)
-        #load the path of the clips
-        self.clips = clips2vectormerge(path, split, types, num_views, not_taking)
+        if split != 'Chall':
+            # To load the annotations
+            self.labels_offence_severity, self.labels_action, self.distribution_offence_severity,self.distribution_action, not_taking, self.number_of_actions = label2vectormerge(path, split, num_views)
+            self.clips = clips2vectormerge(path, split, num_views, not_taking)
+            self.distribution_offence_severity = torch.div(self.distribution_offence_severity, len(self.labels_offence_severity))
+            self.distribution_action = torch.div(self.distribution_action, len(self.labels_action))
 
-        self.shift = shift
+            self.weights_offence_severity = torch.div(1, self.distribution_offence_severity)
+            self.weights_action = torch.div(1, self.distribution_action)
+        else:
+            self.clips = clips2vectormerge(path, split, num_views, [])
+
+        # INFORMATION ABOUT SELF.LABELS_OFFENCE_SEVERITY
+        # self.labels_offence_severity => Tensor of size of the dataset. 
+        # each element of self.labels_offence_severity is another tensor of size 4 (the number of classes) where the value is 1 if it is the class and 0 otherwise
+        # for example if it is not an offence, then the tensor is [1, 0, 0, 0]. 
+
+        # INFORMATION ABOUT SELF.LABELS_ACTION
+        # self.labels_action => Tensor of size of the dataset. 
+        # each element of self.labels_action is another tensor of size 8 (the number of classes) where the value is 1 if it is the class and 0 otherwise
+        # for example if the action is a tackling, then the tensor is [1, 0, 0, 0, 0, 0, 0, 0]. 
+
+        # INFORMATION ABOUT SLEF.CLIPS
+        # self.clips => list of the size of the dataset
+        # each element of the list is another list of size of the number of views. The list contains the paths to all the views of that particular action.
+
+        # The offence_severity groundtruth of the i-th action in self.clips, is the i-th element in the self.labels_offence_severity tensor
+        # The type of action groundtruth of the i-th action in self.clips, is the i-th element in the self.labels_action tensor
+        
         self.split = split
         self.start = start
         self.end = end
@@ -33,23 +47,23 @@ class MultiViewDataset(Dataset):
 
         self.factor = (end - start) / (((end - start) / 25) * fps)
 
-        self.length = len(self.labels_action)
-
-        self.distribution_offence_severity = torch.div(self.distribution_offence_severity, len(self.labels_offence_severity))
-        self.distribution_action = torch.div(self.distribution_action, len(self.labels_action))
-
-        self.weights_offence_severity = torch.div(1, self.distribution_offence_severity)
-        self.weights_action = torch.div(1, self.distribution_action)
-
+        self.length = len(self.clips)
+        print(self.length)
 
     def getDistribution(self):
         return self.distribution_offence_severity, self.distribution_action, 
     def getWeights(self):
         return self.weights_offence_severity, self.weights_action, 
 
-    def __getitem__(self, index):
 
-        frame_shift = random.randint(-self.shift,self.shift)
+    # RETURNS
+    #
+    # self.labels_offence_severity[index][0] => tensor of size 4. Example [1, 0, 0, 0] if the action is not an offence
+    # self.labels_action[index][0] => tensor of size 8.           Example [1, 0, 0, 0, 0, 0, 0, 0] if the type of action is a tackling
+    # videos => tensor of shape V, C, N, H, W with V = number of views, C = number of channels, N = the number of frames, H & W = height & width
+    # self.number_of_actions[index] => the id of the action
+    #
+    def __getitem__(self, index):
 
         prev_views = []
 
@@ -60,6 +74,8 @@ class MultiViewDataset(Dataset):
             if len(prev_views) == 2:
                 continue
 
+            # As we use a batch size > 1 during training, we always randomly select two views even if we have more than two views.
+            # As the batch size during validation and testing is 1, we can have 2, 3 or 4 views per action.
             cont = True
             if self.split == 'Train':
                 while cont:
@@ -69,9 +85,9 @@ class MultiViewDataset(Dataset):
                 index_view = aux
                 prev_views.append(index_view)
 
-            video, _, _ = read_video(self.clips[index][index_view], output_format="THWC")
 
-            frames = video[self.start+frame_shift:self.end+frame_shift,:,:,:]
+            video, _, _ = read_video(self.clips[index][index_view], output_format="THWC")
+            frames = video[self.start:self.end,:,:,:]
 
             final_frames = None
 
@@ -99,11 +115,12 @@ class MultiViewDataset(Dataset):
         if self.num_views != 1 and self.num_views != 5:
             videos = videos.squeeze()   
 
-        #################
         videos = videos.permute(0, 2, 1, 3, 4)
 
-        # size of mvimages: (Views), Channel, Depth, Height, Width
-        return self.labels_offence_severity[index][0], self.labels_action[index][0], videos
+        if self.split != 'Chall':
+            return self.labels_offence_severity[index][0], self.labels_action[index][0], videos, self.number_of_actions[index]
+        else:
+            return -1, -1, videos, str(index)
 
     def __len__(self):
         return self.length
